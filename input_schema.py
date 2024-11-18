5f9bf11eb9b019cfaaefab1a2939c49a42d27f0e
ujjawalPeak01 <119571874+ujjawalPeak01@users.noreply.github.com>
2024-11-18 09:04:07 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,9 @@
+INPUT_SCHEMA = {
+    "TEXT": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["How to make a omellete"]
+    }
+}
+IS_STREAMING_OUTPUT = True
